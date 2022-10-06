4c9bff2c3c3944731e9b28089ccc2d16acd71e97
lll2034006613 <lll2034006613@163.com>
2022-10-06 19:19:18 +0800

    delete test

--- src/com/sxt/GameLauncher.java
+++ src/com/sxt/GameLauncher.java
@@ -2,7 +2,6 @@ package com.sxt;
 
 public class GameLauncher {
     public static void main(String[] args) {
-        // this is me
         GameWindow gameWindow = new GameWindow();
         gameWindow.launch();
     }
