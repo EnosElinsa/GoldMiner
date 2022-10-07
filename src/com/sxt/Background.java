e06e1cf18fe49cb77cc2afe29e27c80ea3c05b4e
Enos <1092903042@qq.com>
2022-10-08 00:19:12 +0800

    Use a better way to get the image in the resources
    This solved the low speed to load the background

--- src/com/sxt/Background.java
+++ src/com/sxt/Background.java
@@ -2,18 +2,20 @@ package com.sxt;
 
 import java.awt.*;
 
+import javax.swing.ImageIcon;
+
 public class Background {
     private Image land;
     private Image levelBackground0;
     private Image floor;
 
     public Background() {
-        land = Toolkit.getDefaultToolkit().getImage("resources/land.png");
-        levelBackground0 = Toolkit.getDefaultToolkit().getImage("resources/level-background-0.png");
-        floor = Toolkit.getDefaultToolkit().getImage("resources/floor_resize.png");
+        land = new ImageIcon("resources/land.png").getImage();
+        levelBackground0 = new ImageIcon("resources/level-background-0.png").getImage();
+        floor = new ImageIcon("resources/floor_resize.png").getImage();
     }
 
-    public void paintBackground(Graphics graphics) {
+    public void drawSelf(Graphics graphics) {
         graphics.drawImage(land, 0, 30, null);
         graphics.drawImage(levelBackground0, 0, land.getHeight(null) + 30, null);
         graphics.drawImage(floor, land.getWidth(null) / 2 - 40, land.getHeight(null) + 8, null);
